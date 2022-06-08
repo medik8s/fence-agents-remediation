34a0d14afb7dedc335a9c96a22904ef5f31da66f
Michael Shitrit <mshitrit@redhat.com>
2022-06-08 16:23:37 +0300

    create FenceAgentsRemediationTemplate CRD
    /home/mshitrit/go/bin/operator-sdk create api --group=fence-agents --version=v1alpha1 --kind=FenceAgentsRemediationTemplate --resource --controller
    
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FenceAgentsRemediation")
 		os.Exit(1)
 	}
+	if err = (&controllers.FenceAgentsRemediationTemplateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "FenceAgentsRemediationTemplate")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
