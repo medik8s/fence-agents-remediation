9cb243f63040fa6660e820264cafde9b16506056
razo7 <oraz@redhat.com>
2023-02-23 10:58:35 +0200

    Add suite test for unit-testing FAR
    New suite and mockExcuter to mock production code using Executor

--- controllers/fenceagentsremediation_controller_test.go
+++ controllers/fenceagentsremediation_controller_test.go
@@ -1,12 +1,44 @@
-package controllers_test
+/*
+Copyright 2023.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package controllers
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 
-	"github.com/medik8s/fence-agents-remediation/controllers"
+	"github.com/medik8s/fence-agents-remediation/pkg/cli"
 )
 
-var _ = Describe("FenceagentsremediationController", func() {
+var (
+	executedCommand []string
+)
 
-})
+// Implements Execute function to mock/test Execute of FenceAgentsRemediationReconciler
+type mockExecuter struct {
+	expected []string
+}
+
+// newMockExecuter is a dummy function for testing
+func newMockExecuter() cli.Executer {
+	mockExpected := []string{"mockExecuter"}
+	mockE := mockExecuter{expected: mockExpected}
+	return &mockE
+}
+
+func (e *mockExecuter) Execute(_ *corev1.Pod, command []string) (stdout string, stderr string, err error) {
+	//store the executed command for testing its validaty
+	executedCommand = command
+	// e.expected = command
+	return "", "", nil
+}
