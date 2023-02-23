1dad0eea684e6f8b8648697ee3c081465f5bdab9
razo7 <oraz@redhat.com>
2023-02-23 10:19:19 +0200

    Bootstrapping a Suite in Controllers
    Using Ginkgo commands to bootsrap a new suite - controllers_test

---
+++ controllers/fenceagentsremediation_controller_test.go
@@ -0,0 +1,12 @@
+package controllers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/medik8s/fence-agents-remediation/controllers"
+)
+
+var _ = Describe("FenceagentsremediationController", func() {
+
+})
