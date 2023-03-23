a954f6c1d3cac3fc6055272154f8af77e751c16e
razo7 <oraz@redhat.com>
2023-03-23 11:47:17 +0200

    Bootstrapping a Suite in test/e2e
    Using Ginkgo commands to bootsrap a new suite - e2e

---
+++ test/e2e/far_e2e_test.go
@@ -0,0 +1,12 @@
+package e2e_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/medik8s/fence-agents-remediation/test/e2e"
+)
+
+var _ = Describe("E2e", func() {
+
+})
