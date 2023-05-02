2c6e157e02fd7f72b3f488e9090ffdc206bd41fb
razo7 <oraz@redhat.com>
2023-05-02 17:38:18 +0300

    Build shared and node parameters for FAR CR based on cluster type
    Automate FA selection and creation based on the platform it use - AWS or BMH

--- test/e2e/far_e2e_test.go
+++ test/e2e/far_e2e_test.go
@@ -3,10 +3,12 @@ package e2e
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
 
 	"github.com/medik8s/fence-agents-remediation/api/v1alpha1"
 	farController "github.com/medik8s/fence-agents-remediation/controllers"
@@ -22,6 +24,9 @@ const (
 	fenceAgentDummyName = "echo"
 	testNamespace       = "openshift-operators"
 	fenceAgentAWS       = "fence_aws"
+	fenceAgentIPMI      = "fence_ipmilan"
+	fenceAgentAction    = "status"
+	nodeIndex           = 0
 
 	// eventually parameters
 	timeoutLogs  = 1 * time.Minute
@@ -30,25 +35,32 @@ const (
 
 var _ = Describe("FAR E2e", func() {
 	var (
-		far        *v1alpha1.FenceAgentsRemediation
-		fenceAgent string
+		far                 *v1alpha1.FenceAgentsRemediation
+		fenceAgent          string
+		clusterPlatformType string
+		clusterPlatform     *configv1.Infrastructure
+		err                 error
 	)
+	BeforeEach(func() {
+		// command -> oc get Infrastructure.config.openshift.io/cluster -o jsonpath='{.spec.platformSpec.type}'
 
-	// command -> oc get Infrastructure.config.openshift.io/cluster -o jsonpath='{.spec.platformSpec.yype}'
-	clusterPlatform, err := farUtils.GetClusterInfo(&configClient)
-	clusterPlatformType := string(clusterPlatform.Spec.PlatformSpec.Type)
-	if err != nil {
-		Fail("can't identify the cluster platform")
-	}
-	log.Info("Clustetr Platform", "type", clusterPlatformType)
+		clusterPlatform, err = farUtils.GetClusterInfo(configClient)
+		clusterPlatformType = string(clusterPlatform.Status.PlatformStatus.Type) //infrastructure.Status.PlatformStatus.Type
+		if err != nil {
+			Fail("can't identify the cluster platform")
+			// fmt.Printf("can't identify the cluster platform")
+		}
+		fmt.Printf("\ncluster name: %s and PlatformType: %s \n", string(clusterPlatform.Name), clusterPlatformType)
+	})
 
 	Context("fence agent - dummy", func() {
 		testNodeName := "dummy-node"
+		fenceAgent = fenceAgentDummyName
 
 		BeforeEach(func() {
 			testShareParam := map[v1alpha1.ParameterName]string{}
 			testNodeParam := map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{}
-			far = createFAR(testNodeName, fenceAgentDummyName, testShareParam, testNodeParam)
+			far = createFAR(testNodeName, fenceAgent, testShareParam, testNodeParam)
 		})
 
 		AfterEach(func() {
@@ -63,52 +75,37 @@ var _ = Describe("FAR E2e", func() {
 
 	Context("fence agent - non-Dummy", func() {
 		//testShareParam,testNodeParam := buildParameters(clusterPlatform, "status")
-
-		if clusterPlatformType == "AWS" {
-			fenceAgent = fenceAgentAWS
-			By("running fence_aws")
-			// } else if clusterPlatformType == "BareMetal"{
-			// 	fenceAgent = fenceAgentIPMI
-			// 	By("running fence_ipmilan")
-		} else {
-			Skip("FAR haven't been tested on this kind of cluster (non AWS or BareMetal)")
-		}
-
-		accessKey, secretKey, err := farUtils.GetAWSCredientals(clientSet)
-		if err != nil {
-			Fail("can't get AWS credentials")
-		}
-
-		// command -> oc get Infrastructure.config.openshift.io/cluster  -o jsonpath='{.status.platformStatus.aws.region}'
-		regionAWS := string(clusterPlatform.Status.PlatformStatus.AWS.Region)
-		actionAWS := "status"
-
-		testShareParam := map[v1alpha1.ParameterName]string{
-			"--access-key": accessKey,
-			"--secret-key": secretKey,
-			"--region":     regionAWS,
-			"--action":     actionAWS,
-			"--verbose":    "",
-		}
-
-		nodeListParam, err := farUtils.GetNodeInfoList(machineClient)
-		if err != nil {
-			Fail("can't get nodes' information- AWS instance ID")
-		}
-		nodeIdentifier := v1alpha1.ParameterName("--plug")
-		testNodeParam := map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{nodeIdentifier: nodeListParam}
-
-		var testNodeName string
-		nodes := &corev1.NodeList{}
-
 		BeforeEach(func() {
+			if clusterPlatformType == "AWS" {
+				fenceAgent = fenceAgentAWS
+				By("running fence_aws")
+			} else if clusterPlatformType == "BareMetal" {
+				fenceAgent = fenceAgentIPMI
+				By("running fence_ipmilan")
+			} else {
+				Skip("FAR haven't been tested on this kind of cluster (non AWS or BareMetal)")
+			}
+
+			testShareParam, err := buildSharedParameters(clusterPlatform, "status")
+			if err != nil {
+				Fail("can't get shared information")
+				// fmt.Printf("can't get nodes' information- AWS instance ID")
+			}
+			testNodeParam, err := buildNodeParameters(clusterPlatformType)
+			if err != nil {
+				Fail("can't get node information")
+				// fmt.Printf("can't get nodes' information- AWS instance ID")
+			}
+			var testNodeName string
+			nodes := &corev1.NodeList{}
+
 			Expect(k8sClient.List(context.Background(), nodes, &client.ListOptions{})).ToNot(HaveOccurred())
 			if len(nodes.Items) <= 1 {
 				Fail("there is one or less available nodes in the cluster")
 			}
 			//TODO: Randomize the node selection
 			// run FA on the first node - a master node
-			nodeObj := nodes.Items[0]
+			nodeObj := nodes.Items[nodeIndex]
 			testNodeName = nodeObj.Name
 			log.Info("Testing Node", "Node name", testNodeName)
 
@@ -127,11 +124,101 @@ var _ = Describe("FAR E2e", func() {
 
 				By("checking the command has been executed successfully")
 				checkFarLogs("ON")
-
 			})
 		})
 	})
 })
+
+func buildSharedParameters(clusterPlatform *configv1.Infrastructure, action string) (map[v1alpha1.ParameterName]string, error) {
+	const (
+		secretAWS = "aws-cloud-credentials"
+		// https://github.com/openshift/release/blob/master/ci-operator/jobs/medik8s/fence-agents-remediation/medik8s-fence-agents-remediation-main-presubmits.yaml#L171
+
+		//AWS
+		// secretAWSOptional = "cluster-secrets-aws"
+		secretKeyAWS = "aws_access_key_id"
+		secretValAWS = "aws_secret_access_key"
+
+		// BareMetal
+		//TODO: secret BM should be based on node name - > oc get bmh -n openshift-machine-api BM_NAME -o jsonpath='{.spec.bmc.credentialsName}'
+		secretBMHExample = "ostest-master-0-bmc-secret"
+		secretKeyBM      = "username"
+		secretValBM      = "password"
+	)
+	var (
+		testShareParam map[v1alpha1.ParameterName]string
+	)
+	clusterPlatformType := string(clusterPlatform.Status.PlatformStatus.Type) //infrastructure.Status.PlatformStatus.Type
+
+	if clusterPlatformType == "AWS" {
+		accessKey, secretKey, err := farUtils.GetCredientals(clientSet, secretAWS, secretKeyAWS, secretValAWS)
+		if err != nil {
+			fmt.Printf("can't get AWS credentials")
+			return nil, err
+		}
+
+		// command -> oc get Infrastructure.config.openshift.io/cluster  -o jsonpath='{.status.platformStatus.aws.region}'
+		regionAWS := string(clusterPlatform.Status.PlatformStatus.AWS.Region)
+
+		testShareParam = map[v1alpha1.ParameterName]string{
+			"--access-key": accessKey,
+			"--secret-key": secretKey,
+			"--region":     regionAWS,
+			"--action":     action,
+			"--verbose":    "",
+		}
+
+	} else if clusterPlatformType == "BareMetal" {
+		// TODO : get ip from GetCredientals
+		// command -> oc get bmh -n openshift-machine-api ostest-master-0 -o jsonpath='{.spec.bmc.address}'
+		// then parse ip
+		username, password, err := farUtils.GetCredientals(clientSet, secretBMHExample, secretKeyBM, secretValBM)
+		if err != nil {
+			fmt.Printf("can't get BM credentials")
+			return nil, err
+		}
+		testShareParam = map[v1alpha1.ParameterName]string{
+			"--username": username,
+			"--password": password,
+			"--ip":       "192.168.111.1",
+			"--action":   action,
+			"--lanplus":  "",
+		}
+	}
+	return testShareParam, nil
+}
+
+func buildNodeParameters(clusterPlatformType string) (map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string, error) {
+
+	var (
+		testNodeParam  map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string
+		nodeListParam  map[v1alpha1.NodeName]string
+		nodeIdentifier v1alpha1.ParameterName
+		err            error
+	)
+
+	if clusterPlatformType == "AWS" {
+		nodeListParam, err = farUtils.GetAWSNodeInfoList(machineClient)
+		if err != nil {
+			// Fail("can't get nodes' information- AWS instance ID")
+			fmt.Printf("can't get nodes' information - AWS instance ID")
+			return nil, err
+		}
+		nodeIdentifier = v1alpha1.ParameterName("--plug")
+
+	} else if clusterPlatformType == "BareMetal" {
+		nodeListParam, err = farUtils.GetBMNodeInfoList(machineClient)
+		if err != nil {
+			// Fail("can't get nodes' information- AWS instance ID")
+			fmt.Printf("can't get nodes' information - ports")
+			return nil, err
+		}
+		nodeIdentifier = v1alpha1.ParameterName("--ipport")
+	}
+	testNodeParam = map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{nodeIdentifier: nodeListParam}
+
+	return testNodeParam, nil
+}
 
 // createFAR assigns the input to FenceAgentsRemediation object, creates CR, and returns the CR object
 func createFAR(nodeName string, agent string, sharedParameters map[v1alpha1.ParameterName]string, nodeParameters map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string) *v1alpha1.FenceAgentsRemediation {
--- test/e2e/utils/cluster.go
+++ test/e2e/utils/cluster.go
@@ -4,12 +4,12 @@ package utils
 
 import (
 	"context"
+	"fmt"
 
-	b64 "encoding/base64"
 	// "encoding/json"
 
 	"github.com/medik8s/fence-agents-remediation/api/v1alpha1"
-	v1 "github.com/openshift/api/config/v1"
+	configv1 "github.com/openshift/api/config/v1"
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
 	machineclient "github.com/openshift/machine-api-operator/pkg/generated/clientset/versioned/typed/machine/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,15 +18,18 @@ import (
 
 const (
 	machineNamespace = "openshift-machine-api"
-	secretAWS        = "aws-cloud-credentials"
 )
 
 // GetClusterInfo fetch the cluster's infrastructure object to identify it's type
-func GetClusterInfo(config *configclient.Interface) (*v1.Infrastructure, error) {
+func GetClusterInfo(config configclient.Interface) (*configv1.Infrastructure, error) {
 	// oc get Infrastructure.config.openshift.io/cluster
 
-	c := *config
-	clusterInfra, err := c.ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
+	// clusterList, _ := config.ConfigV1().Infrastructures().List(context.Background(), metav1.ListOptions{})
+	// for _, cluster := range clusterList.Items {
+	// 	fmt.Printf("\ncluster name:%s and PlatformType: %s \n", string(cluster.Name), string(cluster.Status.PlatformStatus.Type))
+	// }
+
+	clusterInfra, err := config.ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -34,33 +37,32 @@ func GetClusterInfo(config *configclient.Interface) (*v1.Infrastructure, error) 
 }
 
 // GetAWSCredientals searches for AWS secret, and then returns it decoded
-func GetAWSCredientals(clientSet *kubernetes.Clientset) (string, string, error) {
+func GetCredientals(clientSet *kubernetes.Clientset, secretName, secretKey, secretVal string) (string, string, error) {
 	// oc get secrets -n openshift-machine-api aws-cloud-credentials -o jsonpath='{.data.aws_access_key_id}' | base64 -d
 	// oc get secrets -n openshift-machine-api aws-cloud-credentials -o jsonpath='{.data.aws_secret_access_key}' | base64 -d
 
-	awsSecret, err := clientSet.CoreV1().Secrets(machineNamespace).Get(context.Background(), secretAWS, metav1.GetOptions{})
+	secret, err := clientSet.CoreV1().Secrets(machineNamespace).Get(context.Background(), secretName, metav1.GetOptions{})
 	if err != nil {
 		return "", "", err
 	}
-	keyID := b64.StdEncoding.EncodeToString([]byte(awsSecret.Data["aws_access_key_id"]))
-	keySecret := b64.StdEncoding.EncodeToString([]byte(awsSecret.Data["aws_secret_access_key"]))
-	// // Parse the AWS credentials - AWS access key and secret key
+	fmt.Printf("Key: %s Value: %s \n", secret.Data[secretKey], secret.Data[secretVal])
+	// Parse the AWS credentials - AWS access key and secret key
 	// var credentials v1beta1credential.OpenShiftCredentialProvider
 	// err = json.Unmarshal(awsSecret.Data["credentials"], &credentials)
 	// if err != nil {
 	// 	return "","", err
 	// }
 	// return credentials.Spec.SecretRef.KeyValues["access_key_id"], credentials.Spec.SecretRef.KeyValues["secret_access_key"], nil
-	return keyID, keySecret, nil
+	// return keyID, keySecret, nil
+	return string(secret.Data[secretKey]), string(secret.Data[secretVal]), nil
 }
 
-// GetNodeInfoList returns a list of the node names and their identification, e.g., AWS instance ID
-func GetNodeInfoList(machineClient *machineclient.MachineV1beta1Client) (map[v1alpha1.NodeName]string, error) {
+// GetAWSNodeInfoList returns a list of the node names and their identification, e.g., AWS instance ID
+func GetAWSNodeInfoList(machineClient *machineclient.MachineV1beta1Client) (map[v1alpha1.NodeName]string, error) {
 	//  oc get machine -n openshift-machine-api MACHINE_NAME -o jsonpath='{.spec.providerID}'
 	//  oc get machine -n openshift-machine-api MACHINE_NAME -o jsonpath='{.status.nodeRef.name}'
 
-	var nodeList map[v1alpha1.NodeName]string
-
+	nodeList := make(map[v1alpha1.NodeName]string)
 	// Get the list of Machines in the openshift-machine-api namespace
 	machineList, err := machineClient.Machines("openshift-machine-api").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
@@ -70,7 +72,34 @@ func GetNodeInfoList(machineClient *machineclient.MachineV1beta1Client) (map[v1a
 	// creates map for nodeName and AWS instance ID
 	for _, machine := range machineList.Items {
 		nodeName := v1alpha1.NodeName(string(machine.Status.NodeRef.Name))
+		fmt.Printf("node: %s Instance ID: %s \n", nodeName, string(*machine.Spec.ProviderID))
 		nodeList[nodeName] = string(*machine.Spec.ProviderID)
 	}
+	return nodeList, nil
+}
+
+// GetNodeInfoList returns a list of the node names and their identification, e.g., AWS instance ID
+func GetBMNodeInfoList(machineClient *machineclient.MachineV1beta1Client) (map[v1alpha1.NodeName]string, error) {
+
+	//TODO: seacrch for BM and fetch ports
+
+	nodeList := map[v1alpha1.NodeName]string{
+		"master-0": "6230",
+		"master-1": "6231",
+		"master-2": "6232",
+		"worker-0": "6233",
+		"worker-1": "6234",
+		"worker-2": "6235",
+	}
+	// testNodeParam = map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{
+	// 	"--ipport": {
+	// 		"master-0": "6230",
+	// 		"master-1": "6231",
+	// 		"master-2": "6232",
+	// 		"worker-0": "6233",
+	// 		"worker-1": "6234",
+	// 		"worker-2": "6235",
+	// 	},
+	// }
 	return nodeList, nil
 }
