b3de5887529ea38c80137bb44df3e1fa48aec355
razo7 <oraz@redhat.com>
2023-05-22 17:30:26 +0300

    Wrap errors rather than log them twice
    Wrapping the errors from GetFenceAgentsRemediationPod and log the error once in main or in checkFarLogs

--- pkg/utils/pods.go
+++ pkg/utils/pods.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	corev1 "k8s.io/api/core/v1"
@@ -9,26 +10,23 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // GetFenceAgentsRemediationPod fetches the FAR pod based on FAR's label and namespace
 func GetFenceAgentsRemediationPod(r client.Reader) (*corev1.Pod, error) {
-	logger := ctrl.Log.WithName("utils-pods")
+	var podNamespace string
 	pods := &corev1.PodList{}
 	selector := labels.NewSelector()
 	requirement, _ := labels.NewRequirement("app", selection.Equals, []string{"fence-agents-remediation-operator"})
 	selector = selector.Add(*requirement)
-	var podNamespace string
 	podNamespace, err := GetDeploymentNamespace()
 	if err != nil {
-		logger.Error(err, "failed fetching FAR namespace")
+		return nil, fmt.Errorf("failed fetching FAR namespace - %w", err)
 	}
 	err = r.List(context.Background(), pods, &client.ListOptions{LabelSelector: selector, Namespace: podNamespace})
 	if err != nil {
-		logger.Error(err, "failed fetching FAR pod")
-		return nil, err
+		return nil, fmt.Errorf("failed fetching FAR pod - %w", err)
 	}
 	if len(pods.Items) == 0 {
 		podNotFoundErr := &apiErrors.StatusError{ErrStatus: metav1.Status{
@@ -36,9 +34,7 @@ func GetFenceAgentsRemediationPod(r client.Reader) (*corev1.Pod, error) {
 			Code:   http.StatusNotFound,
 			Reason: metav1.StatusReasonNotFound,
 		}}
-		logger.Error(podNotFoundErr, "No Fence Agent pods were found")
-		return nil, podNotFoundErr
+		return nil, fmt.Errorf("no Fence Agent pods were found - %w", podNotFoundErr)
 	}
-
 	return &pods.Items[0], nil
 }
