0c91ae1e24189419f33166d64eb3dce7d77b1c1f
razo7 <oraz@redhat.com>
2023-08-01 13:18:40 +0300

    Add three status conditions for FAR CR
    Processing, FenceAgentActionSucceeded, and Succeeded status conditions have been added to verify FAR remediation status

---
+++ pkg/utils/errors.go
@@ -0,0 +1,9 @@
+package utils
+
+const (
+	// condition status error messages
+	NoFenceAgentsRemediationCRFound = "noFenceAgentsRemediationCRFound"
+	ConditionNotSetError            = "ConditionNotSet"
+	ConditionSetButNoMatchError     = "ConditionSetButNoMatch"
+	ConditionSetAndMatchSuccess     = "ConditionSetAndMatchSuccess"
+)
