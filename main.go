cdd2d43a02625fa3cafd7c88678777f4f563e368
Carlo Lobrano <c.lobrano@gmail.com>
2023-10-20 09:58:48 +0200

    Fix Manager configuration after controller-runtime update
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	"github.com/medik8s/fence-agents-remediation/api/v1alpha1"
 	"github.com/medik8s/fence-agents-remediation/controllers"
@@ -75,10 +76,13 @@ func main() {
 
 	printVersion()
 
+	metricsOpts := server.Options{
+		BindAddress: metricsAddr,
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Metrics:                metricsOpts,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "cb305759.medik8s.io",
